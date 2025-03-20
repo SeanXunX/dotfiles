c1eca1969c5d25ea04d9b470de441c4d7bd651c2
sean <seanxunx@outlook.com>
2025-03-20 21:27:43 +0800

    add rust lsp for nvim

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -44,5 +44,29 @@ return {
         -- clangd
         lspconfig.clangd.setup({})
 
+        lspconfig.rust_analyzer.setup({
+            on_attach = function(client, bufnr)
+                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+            end,
+            settings = {
+                ["rust-analyzer"] = {
+                    imports = {
+                        granularity = {
+                            group = "module",
+                        },
+                        prefix = "self",
+                    },
+                    cargo = {
+                        buildScripts = {
+                            enable = true,
+                        },
+                    },
+                    procMacro = {
+                        enable = true
+                    },
+                }
+            }
+        })
+
     end
 }
